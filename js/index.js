e8df18d0d93819227edd6c183bd3ee89509757e1
wxywr <1261046195@qq.com>
2017-04-10 13:34:19 +0800

    update

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ $(window).resize(function(){
 
 $(function(){
 	$('html').css({'font-size':$(window).width()/3.2+"px"});
+	
 	$('nav .swiper-slide').click(function(){
 		var index = $(this).index();
 		$('.tab').eq(index).css('display','block').siblings('.tab').css('display','none');
